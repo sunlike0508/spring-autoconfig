28a8b8f526c3df77aa87ab20c5bf55d782afa3b6
seonhoshin <sunlike0508@gmail.com>
2024-11-26 22:52:36 +0900

    자동구성

--- src/main/java/hello/config/DbConfig.java
+++ src/main/java/hello/config/DbConfig.java
@@ -11,7 +11,7 @@ import org.springframework.jdbc.support.JdbcTransactionManager;
 import org.springframework.transaction.TransactionManager;
 
 @Slf4j
-@Configuration
+//@Configuration
 public class DbConfig {
 
     @Bean
